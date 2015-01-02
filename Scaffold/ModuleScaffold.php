cc5589d37021b2b0e05f92f7bd218b85dfd978b3
Nicolas Widart <n.widart@gmail.com>
2015-01-02 13:48:44 +0100

    Adding basic setters

--- Scaffold/ModuleScaffold.php
+++ Scaffold/ModuleScaffold.php
@@ -2,8 +2,75 @@
 
 class ModuleScaffold
 {
+    /**
+     * Contains the vendor name
+     * @var string
+     */
+    protected $vendor;
+    /**
+     * Contains the Module name
+     * @var string
+     */
+    protected $name;
+    /**
+     * Contains an array of entities to generate
+     * @var array
+     */
+    protected $entities;
+    /**
+     * Contains an array of value objects to generate
+     * @var array
+     */
+    protected $valueObjects;
+
+    /**
+     *
+     */
     public function scaffold()
     {
+    }
 
+    /**
+     * @param string $vendor
+     * @return $this
+     */
+    public function vendor($vendor)
+    {
+        $this->vendor = $vendor;
+
+        return $this;
+    }
+
+    /**
+     * @param string $name
+     * @return $this
+     */
+    public function name($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * @param array $entities
+     * @return $this
+     */
+    public function withEntities(array $entities)
+    {
+        $this->entities = $entities;
+
+        return $this;
+    }
+
+    /**
+     * @param array $valueObjects
+     * @return $this
+     */
+    public function withValueObjects(array $valueObjects)
+    {
+        $this->valueObjects = $valueObjects;
+
+        return $this;
     }
 }

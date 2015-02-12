493e05d465589713596c8a531ccf4e5a1c996deb
Nicolas Widart <n.widart@gmail.com>
2015-02-12 20:54:56 +0100

    Use the new route file notation

---
+++ Http/backendRoutes.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Routing\Router;
+
+$router->group(['prefix' => '/workshop'],
+    function (Router $router) {
+        $router->get('modules', ['as' => 'admin.workshop.modules.index', 'uses' => 'ModulesController@index']);
+        $router->post('modules', ['as' => 'admin.workshop.modules.store', 'uses' => 'ModulesController@store']);
+        # Workbench
+        $router->get('workbench', ['as' => 'admin.workshop.workbench.index', 'uses' => 'WorkbenchController@index']);
+        $router->post('generate', ['as' => 'admin.workshop.workbench.generate.index', 'uses' => 'WorkbenchController@generate']);
+        $router->post('migrate', ['as' => 'admin.workshop.workbench.migrate.index', 'uses' => 'WorkbenchController@migrate']);
+        $router->post('install', ['as' => 'admin.workshop.workbench.install.index', 'uses' => 'WorkbenchController@install']);
+        $router->post('seed', ['as' => 'admin.workshop.workbench.seed.index', 'uses' => 'WorkbenchController@seed']);
+    }
+);

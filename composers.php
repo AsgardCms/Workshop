c97fcf378140b52e2e360c34cff8627d6a5932fc
Nicolas Widart <n.widart@gmail.com>
2015-02-13 13:49:00 +0100

    Use the new Admin theme for administration

--- composers.php
+++ composers.php
@@ -1,6 +1,6 @@
 <?php
 
-View::composer('core::partials.sidebar-nav', 'Modules\Workshop\Composers\SidebarViewComposer');
+View::composer('partials.sidebar-nav', 'Modules\Workshop\Composers\SidebarViewComposer');
 View::composer([
         'workshop::admin.workbench.tabs.migrate',
         'workshop::admin.workbench.tabs.seed',

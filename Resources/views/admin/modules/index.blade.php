159dff619f46ae1cee3347d929ea6a2da36c84d2
Nicolas Widart <n.widart@gmail.com>
2015-02-18 20:26:00 +0100

    Divide the module list in 2

--- Resources/views/admin/modules/index.blade.php
+++ Resources/views/admin/modules/index.blade.php
@@ -18,27 +18,29 @@
 {!! Form::open(['route' => 'admin.workshop.modules.store', 'method' => 'post']) !!}
 <div class="row">
     <div class="col-md-12">
-        <div class="nav-tabs-custom">
-            <ul class="nav nav-tabs">
-                <li class="active"><a href="#tab_1-1" data-toggle="tab">{{ trans('workshop::modules.tab.module list') }}</a></li>
-            </ul>
-            <div class="tab-content">
-                <div class="tab-pane active" id="tab_1-1">
-                    <ul>
-                        @foreach($modules as $module)
-                            <li>
-                                <div class="checkbox">
-                                    <label for="{{ $module }}">
-                                        <input id="{{ $module }}" name="modules[{{ $module }}]" type="checkbox" class="flat-blue" <?php echo Module::active($module) ? 'checked' : '' ?> <?php echo isset($coreModules[$module->getName()]) ? 'disabled' : ''; ?> value="true" /> {{ $module }}
-                                    </label>
-                                </div>
-                            </li>
-                        @endforeach
-                    </ul>
-                    <div class="box-footer">
-                        <button type="submit" class="btn btn-primary btn-flat">{{ trans('workshop::modules.button.save module configuration') }}</button>
-                    </div>
+        <div class="box box-primary">
+            <div class="box-body">
+                <div class="row">
+                    <?php foreach($modules->chunk((int)ceil($modules->count()/2)) as $moduleChunk): ?>
+                        <div class="col-md-6">
+                            <ul class="list-unstyled">
+                                @foreach($moduleChunk as $module)
+                                    <li>
+                                        <div class="checkbox">
+                                            <label for="{{ $module }}">
+                                                <input id="{{ $module }}" name="modules[{{ $module }}]" type="checkbox" class="flat-blue" <?php echo Module::active($module) ? 'checked' : '' ?> <?php echo isset($coreModules[$module->getName()]) ? 'disabled' : ''; ?> value="true" /> {{ $module }}
+                                            </label>
+                                        </div>
+                                    </li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    <?php endforeach; ?>
                 </div>
+            </div>
+
+            <div class="box-footer">
+                <button type="submit" class="btn btn-primary btn-flat">{{ trans('workshop::modules.button.save module configuration') }}</button>
             </div>
         </div>
     </div>

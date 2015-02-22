88b0237a8f585b3ecfa8f7cad8be453efbf95aa7
Nicolas Widart <n.widart@gmail.com>
2015-02-22 11:55:23 +0100

    Add tests for require dev and autoload dev

--- Tests/ModuleScaffoldTest.php
+++ Tests/ModuleScaffoldTest.php
@@ -361,4 +361,28 @@ class ModuleScaffoldTest extends BaseTestCase
 
         $this->assertTrue(isset($composerJson->require->$key));
     }
+
+    /** @test */
+    public function it_should_add_require_dev_to_composer_json()
+    {
+        $this->scaffoldModuleWithEloquent();
+
+        $composerJson = $this->finder->get($this->testModulePath . '/composer.json');
+        $composerJson = json_decode($composerJson);
+        $key = 'require-dev';
+
+        $this->assertTrue(isset($composerJson->$key));
+    }
+
+    /** @test */
+    public function it_should_add_autoload_dev_to_composer_json()
+    {
+        $this->scaffoldModuleWithEloquent();
+
+        $composerJson = $this->finder->get($this->testModulePath . '/composer.json');
+        $composerJson = json_decode($composerJson);
+        $key = 'autoload-dev';
+
+        $this->assertTrue(isset($composerJson->$key));
+    }
 }

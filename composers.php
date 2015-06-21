47d0fe156499d53663b587acb062b8f7905d2688
Patrick Brouwers <patrickbrouwersfilm@gmail.com>
2015-06-21 19:42:13 +0200

    Workshop sidebar extender

--- composers.php
+++ composers.php
@@ -1,6 +1,5 @@
 <?php
 
-View::composer('partials.sidebar-nav', 'Modules\Workshop\Composers\SidebarViewComposer');
 View::composer([
         'workshop::admin.workbench.tabs.migrate',
         'workshop::admin.workbench.tabs.seed',

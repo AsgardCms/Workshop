a8d608b1ef3c4f5bac351d3e8e7ed58c3726a235
Nicolas Widart <n.widart@gmail.com>
2015-06-25 16:19:10 +0200

    Preparing the theme scaffold command

---
+++ Console/ThemeScaffoldCommand.php
@@ -0,0 +1,26 @@
+<?php namespace Modules\Workshop\Console;
+
+use Illuminate\Console\Command;
+use Modules\Workshop\Scaffold\Theme\ThemeScaffold;
+
+class ThemeScaffoldCommand extends Command
+{
+    protected $signature = 'asgard:theme:scaffold {name}';
+    protected $description = 'Scaffold a new theme';
+    /**
+     * @var ThemeScaffold
+     */
+    private $themeScaffold;
+
+    public function __construct(ThemeScaffold $themeScaffold)
+    {
+        parent::__construct();
+        $this->themeScaffold = $themeScaffold;
+    }
+
+    public function fire()
+    {
+        $themeName = $this->argument('name');
+        $this->info("Generating a fresh theme called [$themeName] ...");
+    }
+}

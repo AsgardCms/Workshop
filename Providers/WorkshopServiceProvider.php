6a3e53bd2d8a2b7789be65def108acef0c50cb62
Nicolas Widart <n.widart@gmail.com>
2015-06-25 16:19:25 +0200

    Registering the command

--- Providers/WorkshopServiceProvider.php
+++ Providers/WorkshopServiceProvider.php
@@ -3,11 +3,14 @@
 use Illuminate\Support\ServiceProvider;
 use Modules\Core\Services\Composer;
 use Modules\Workshop\Console\ModuleScaffoldCommand;
+use Modules\Workshop\Console\ThemeScaffoldCommand;
 use Modules\Workshop\Console\UpdateModuleCommand;
 use Modules\Workshop\Scaffold\Module\Generators\EntityGenerator;
 use Modules\Workshop\Scaffold\Module\Generators\FilesGenerator;
 use Modules\Workshop\Scaffold\Module\Generators\ValueObjectGenerator;
 use Modules\Workshop\Scaffold\Module\ModuleScaffold;
+use Modules\Workshop\Scaffold\Theme\ThemeGeneratorFactory;
+use Modules\Workshop\Scaffold\Theme\ThemeScaffold;
 
 class WorkshopServiceProvider extends ServiceProvider
 {
@@ -33,19 +36,21 @@ class WorkshopServiceProvider extends ServiceProvider
      */
     private function registerCommands()
     {
-        $this->registerScaffoldCommand();
+        $this->registerModuleScaffoldCommand();
         $this->registerUpdateCommand();
+        $this->registerThemeScaffoldCommand();
 
         $this->commands([
             'command.asgard.module.scaffold',
             'command.asgard.module.update',
+            'command.asgard.theme.scaffold',
         ]);
     }
 
     /**
      * Register the scaffold command
      */
-    private function registerScaffoldCommand()
+    private function registerModuleScaffoldCommand()
     {
         $this->app->singleton('asgard.module.scaffold', function ($app) {
             return new ModuleScaffold(
@@ -69,6 +74,20 @@ class WorkshopServiceProvider extends ServiceProvider
     {
         $this->app->bindShared('command.asgard.module.update', function ($app) {
             return new UpdateModuleCommand(new Composer($app['files'], base_path()));
+        });
+    }
+
+    /**
+     * Register the theme scaffold command
+     */
+    private function registerThemeScaffoldCommand()
+    {
+        $this->app->singleton('asgard.theme.scaffold', function ($app) {
+            return new ThemeScaffold(new ThemeGeneratorFactory(), $app['files']);
+        });
+
+        $this->app->bindShared('command.asgard.theme.scaffold', function ($app) {
+            return new ThemeScaffoldCommand($app['asgard.theme.scaffold']);
         });
     }
 }
---
+++ Tests/ThemeScaffoldTest.php
@@ -0,0 +1,84 @@
+<?php namespace Modules\Workshop\Tests;
+
+use Modules\Workshop\Scaffold\Theme\Exceptions\FileTypeNotFoundException;
+use Modules\Workshop\Scaffold\Theme\Exceptions\ThemeExistsException;
+use Modules\Workshop\Scaffold\Theme\ThemeScaffold;
+
+class ThemeScaffoldTest extends BaseTestCase
+{
+    public $path = 'Themes';
+
+    /**
+     * @var ThemeScaffold
+     */
+    protected $scaffold;
+
+    /**
+     * @var \Illuminate\Filesystem\Filesystem
+     */
+    protected $finder;
+    /**
+     * @var
+     */
+    protected $testThemeName;
+    /**
+     * @var
+     */
+    protected $testThemePath;
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->finder = $this->app['files'];
+        $this->scaffold = $this->app['asgard.theme.scaffold'];
+        if (!$this->finder->isDirectory(base_path("Themes"))) {
+            $this->finder->makeDirectory(base_path("Themes"));
+        }
+        $this->testThemeName = 'TestingTheme';
+        $this->testThemePath = base_path("Themes/{$this->testThemeName}");
+    }
+
+    public function tearDown()
+    {
+        $this->finder->deleteDirectory($this->testThemePath);
+        $this->finder->deleteDirectory(base_path("Themes"));
+    }
+
+    /** @test */
+    public function it_generates_theme_folder()
+    {
+        $this->scaffold->setFiles([]);
+
+        $this->scaffold->generate($this->testThemeName);
+
+        $this->assertTrue($this->finder->isDirectory($this->testThemePath));
+    }
+
+    /** @test */
+    public function it_throws_exception_if_file_type_does_not_exist()
+    {
+        $this->setExpectedException(FileTypeNotFoundException::class);
+
+        $this->scaffold->generate($this->testThemeName);
+    }
+
+    /** @test */
+    public function it_throws_exception_if_theme_exists()
+    {
+        $this->setExpectedException(ThemeExistsException::class);
+
+        $this->scaffold->setFiles([]);
+        $this->scaffold->generate($this->testThemeName);
+        $this->scaffold->generate($this->testThemeName);
+    }
+
+    /** @test */
+    public function it_creates_theme_json_file()
+    {
+        $this->scaffold->setFiles(['themeJson']);
+
+        $this->scaffold->generate($this->testThemeName);
+
+        $this->finder->isFile($this->testThemePath . '/theme.json');
+    }
+}

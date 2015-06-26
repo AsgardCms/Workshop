05efa62537675f0dc41533cadddd918ef4e8d04b
Nicolas Widart <n.widart@gmail.com>
2015-06-26 17:38:02 +0200

    Hooking the theme scaffold command with the ThemeScaffold class

--- Console/ThemeScaffoldCommand.php
+++ Console/ThemeScaffoldCommand.php
@@ -20,7 +20,27 @@ class ThemeScaffoldCommand extends Command
 
     public function fire()
     {
-        $themeName = $this->argument('name');
-        $this->info("Generating a fresh theme called [$themeName] ...");
+        $themeName = $this->ask('Please enter the theme name in the following format: vendor/name');
+        list($vendor, $name) = $this->separateVendorAndName($themeName);
+        $type = $this->choice('Would you like to create a front end or backend theme ?', ['Frontend', 'Backend'], 0);
+
+        $this->themeScaffold->setName($name)->setVendor($vendor)->forType(strtolower($type))->generate();
+
+        $this->info("Generated a fresh theme called [$themeName]. You'll find it in the Themes/ folder");
+    }
+
+    /**
+     * Extract the vendor and module name as two separate values
+     * @param  string $fullName
+     * @return array
+     */
+    private function separateVendorAndName($fullName)
+    {
+        $explodedFullName = explode('/', $fullName);
+
+        return [
+            $explodedFullName[0],
+            ucfirst($explodedFullName[1]),
+        ];
     }
 }

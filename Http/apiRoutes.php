cc3c5e24f3acce183fd9f88374cb1e00dbd99404
Nicolas Widart <n.widart@gmail.com>
2015-07-10 16:35:39 +0200

    Adding Api controller for publishing theme assets

--- Http/apiRoutes.php
+++ Http/apiRoutes.php
@@ -1,4 +1,5 @@
 <?php
 
 
-$router->post('/workshop/modules/{module}/publish', ['as' => 'api.workshop.module.publish', 'uses' => 'ModulesController@publishAssets']);
+post('/workshop/modules/{module}/publish', ['as' => 'api.workshop.module.publish', 'uses' => 'ModulesController@publishAssets']);
+post('/workshop/themes/{theme}/publish', ['as' => 'api.workshop.theme.publish', 'uses' => 'ThemeController@publishAssets']);

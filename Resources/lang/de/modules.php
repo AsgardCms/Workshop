08a8377101cd2c73ef055716282b4bc707b8c13e
Nuno Ribeiro <nmpribeiro@gmail.com>
2015-08-20 15:24:43 +0000

    Added de locale and pt

---
+++ Resources/lang/de/modules.php
@@ -0,0 +1,34 @@
+<?php
+
+return [
+    'title' => 'Modulverwaltung',
+    'breadcrumb' => [
+        'modules' => 'Module',
+    ],
+    'tab' => [
+        'module list' => 'Modulliste',
+    ],
+    'button' => [
+        'save module configuration' => 'Modulkonfiguration speichern',
+    ],
+    'table' => [
+        'vendor' => 'Vendor',
+        'name' => 'Namen',
+        'enabled' => 'Aktiviert',
+        'version' => 'Version',
+    ],
+    'enabled' => 'Aktiviert',
+    'enable' => 'Aktiviert',
+    'disabled' => 'Behindert',
+    'disable' => 'Behindert',
+    'viewing module' => 'Modul Anzeigen',
+    'module' => 'Modul',
+    'publish assets' => 'Veröffentlichen Vermögenswerte',
+    'module enabled' => 'Modul Aktiviert.',
+    'module disabled' => 'Modul Behindert.',
+    'module cannot be disabled' => 'Dies ist ein Kernmodul, es kann nicht deaktiviert werden.',
+    'changelog' => 'Änderungsprotokoll',
+    'added' => 'Hinzugefügt',
+    'changed' => 'Geändert',
+    'removed' => 'Entfernt',
+];
